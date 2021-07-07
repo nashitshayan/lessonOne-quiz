a4e6e3cf497b731bdbc39fa7a314dc5ccbce825b
nashitshayan <84833473+nashitshayan@users.noreply.github.com>
2021-07-07 08:59:21 +0530

    JS bonus exercise

---
+++ app.js
@@ -0,0 +1,46 @@
+// var userName = document.querySelector("#input");
+
+// var bday = document.querySelector("#bday");
+// var submit = document.querySelector("#btn-submit");
+
+
+
+
+
+// // if(isNaN(bDate[2])|| isNaN(bDate[1])|| isNaN(bDate[0]))
+// // {
+// //     alert()
+// // }
+
+
+// function leapYear() {
+
+//     var bDate = bday.value.split('-');
+
+//     var bY = bDate[0];
+//     var bM = bDate[1];
+//     var bD = bDate[2];
+
+
+//     if (bY % 4 == 0) {
+//         if (bY % 100 == 0) {
+//             if(bY%400==0)
+//             {
+//                 alert("You were born in a leap year!")
+//             }
+//         }
+//         else{
+//             alert("You were born in a leap year!")
+//         }
+//     }
+//     else{
+//         alert("meh, you weren't born in a leap year!")
+//     }
+
+// }
+
+// bday.addEventListener("change", leapYear)
+
+
+
+
